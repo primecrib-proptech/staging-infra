277d0b38b843b02755b5e1558a1bed58faff1393
John Adeshola <johna@aku.africa>
2025-11-08 13:27:39 +0100

    Updates

--- postgres/init.sh
+++ postgres/init.sh
@@ -9,5 +9,14 @@ until pg_isready -U postgres; do
   sleep 2
 done
 
+# Run the SQL initialization template with variable substitution
 psql -v DB_PASSWORD="$DB_PASSWORD" \
      -f /docker-entrypoint-initdb.d/init.sql.template
+
+
+## Run the SQL initialization template with variable substitution
+ #psql -U postgres -v PROPTECH_PASSWORD="$PROPTECH_PASSWORD" \
+ #     -v AUDIT_PASSWORD="$AUDIT_PASSWORD" \
+ #     -v VAULT_PASSWORD="$VAULT_PASSWORD" \
+ #     -v QUARTZ_PASSWORD="$QUARTZ_PASSWORD" \
+ #     -f /docker-entrypoint-initdb.d/init.sql.template
