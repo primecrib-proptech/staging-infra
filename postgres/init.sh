0bb0a8d1119e9ab01351840abc5249d400a24092
John Adeshola <johna@aku.africa>
2025-11-08 13:15:15 +0100

    Updates

--- postgres/init.sh
+++ postgres/init.sh
@@ -3,5 +3,11 @@ set -eu
 
 export DB_PASSWORD=$(cat /run/secrets/db_password)
 
+# Wait for PostgreSQL to be ready
+until pg_isready -U postgres; do
+  echo "Waiting for PostgreSQL to start..."
+  sleep 2
+done
+
 psql -v DB_PASSWORD="$DB_PASSWORD" \
      -f /docker-entrypoint-initdb.d/init.sql.template
