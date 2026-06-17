9913fd484453e01f0c07474600897993ef5b10eb
John Adeshola <john.adeshola@beeboard.eu>
2026-06-17 16:12:26 +0100

    Updates

--- postgres/init.sh
+++ postgres/init.sh
@@ -12,7 +12,7 @@ read_secret() {
   cat "$file"
 }
 
-export DB_OWNER_PASSWORD=$(read_secret /run/secrets/db_password)
+export DB_PASSWORD=$(read_secret /run/secrets/db_password)
 export DB_RO_PASSWORD=$(read_secret /run/secrets/db_ro_password)
 export DB_APP_PASSWORD=$(read_secret /run/secrets/db_app_password)
 export DB_API_PASSWORD=$(read_secret /run/secrets/db_api_password)
@@ -24,7 +24,7 @@ until pg_isready -U postgres; do
 done
 
 psql -U postgres -d postgres \
-    -v DB_OWNER_PASSWORD="$DB_OWNER_PASSWORD" \
+    -v DB_PASSWORD="$DB_PASSWORD" \
     -v DB_RO_PASSWORD="$DB_RO_PASSWORD" \
     -v DB_APP_PASSWORD="$DB_APP_PASSWORD" \
     -v DB_API_PASSWORD="$DB_API_PASSWORD" \
